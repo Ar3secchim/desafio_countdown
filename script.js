ac1bb04192a674671e97a0316477c1cb0f832173
Renara Secchim <renarasecchim@gmail.com>
2022-02-24 11:33:26 -0300

    update_feature_countdown

--- script.js
+++ script.js
@@ -1,38 +1,26 @@
-const days = document.getElementById("day_time");
-const hours = document.getElementById("hours_time");
-const mins = document.getElementById("min_time");
-const seconds = document.getElementById("sec_time");
 
-const newNiver = "4 fev 2023";
+const countdown = () =>{
 
-function countdown(){
+  const countDate = new Date("mar 07, 2022 19:00:00").getTime(); //getTime()retorna o número de milissegundos
+  const now = new Date() .getTime(); //date now
+  const gap = countDate - now; //diference a dates 
+  
+  //How the does time work?
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
-    const newNiverDate = new Date(newNiver);
-    const curretDate = new Date();
-
-    const totalSeconds = (newNiverDate - curretDate)/1000;
-
-    const days = Math.floor(totalSeconds / 3600 /24);
-
-    const hours = Math.floor(totalSeconds / 3600) % 24;
-
-    const mins = Math.floor(totalSeconds / 60) %60;
-
-    const seconds = Math.floor(totalSeconds) %60;
-
-    days.innerHTML = days;
-    hours.innerHTML = formatTime(hours) ;
-    mins.innerHTML = formatTime(mins);
-    seconds.innerHTML = formatTime(seconds);
-
-
+  //calculate o gap 
+  const textDay = Math.floor(gap / day);
+  const textHour = Math.floor((gap % day) /hour);
+  const textMinute = Math.floor((gap % hour) /minute);
+  const textSecond = Math.floor((gap % minute) /second);
+  
+  document.getElementById("day_time") .innerHTML = textDay;
+  document.getElementById("hours_time") .innerHTML = textHour;
+  document.getElementById("min_time") .innerHTML = textMinute;
+  document.getElementById("sec_time") .innerHTML = textSecond;
 }
 
-
-function formatTime(time){
-        return time < 10 ? `0${time}` : time;
-}
-
-countdown();
-
-setInterval(countdown,1000);
+setInterval(countdown, 1000);//refresh a function at specified intervals
